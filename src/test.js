4b30be6fdb98b3a4fed7055a2baa3f63489aa84b
Lal Bahadur BC <lalbahadurbc2054@gmail.com>
2026-02-05 10:08:22 +0545

    fix: the month days in 2042

--- src/test.js
+++ src/test.js
@@ -1,9 +1,15 @@
 import { ADToBS, BSToAD,isDateInSupportedRange } from "../dist/index.es.js";
 
-console.log(ADToBS(new Date("2025-02-03")));
+console.log(ADToBS(new Date("1987-04-14")));
+console.log(ADToBS(new Date("2026-02-05")));
+
 console.log(BSToAD("2076-05-08"));
 // console.log(formatBS(ADToBS(new Date("2024-01-01"))));
 // console.log({getSupportedDateRange:getSupportedDateRange()})
 
 const isSupported1 = isDateInSupportedRange(new Date("2024-02-03"));
 console.log(isSupported1); // true
+
+
+
+
